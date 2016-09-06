72b5fe2d949e942ffc5c0e37e5f4976d64451d86
Ariel Meneses García <Ariel Meneses Garca@192.168.1.74>
2016-09-06 00:36:16 -0500

    Ariel - Vista terminada , solo falta validar los archivos de Bootstap

--- application/views/Login/index.php
+++ application/views/Login/index.php
@@ -7,5 +7,35 @@
  */
 
 ?>
+/*
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<script src="libs/js/jquery.js" type="text/javascript"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="scripts/login.js"></script>
 
-<h1>hola</h1>
+<div class="container ">
+    <div class="row ">
+           <div class="col-md-offset-5 col-md-3 well">
+             <form>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Usuario:</label>
+                  <input type="text" class="form-control" id="User" placeholder="Usuario">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Contraseña:</label>
+                  <input type="password" class="form-control" id="Password" placeholder="Contarseña">
+                </div>
+                <div class="form-group col-lg-offset-7">
+                    <button id="Ingresar" type="button" class="btn btn-success">Iniciar Sesion</button>
+                </div>
+                 <div id="AnswerLogin" class="alert alert-danger hidden subFormatLogin"></div>
+              </form>
+        
+        </div>
+     
+    </div>
+</div>
