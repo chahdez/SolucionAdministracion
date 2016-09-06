34c32fbdd94454316d06de08da8133e8c1070670
Ariel Meneses García <Ariel Meneses Garca@192.168.1.74>
2016-09-06 00:35:34 -0500

    Ariel - Login terminado , falta solo esperar el servicio para validar informacion

--- scripts/login.js
+++ scripts/login.js
@@ -1,10 +1,47 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 $(function(){
-    alert("ola k ase");
-})
+    document.getElementById('User').onkeypress = function (e) {
+        if (!e) e = window.event;
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == '13') {
+            ValidaSesion();
+        }
+    }
+    document.getElementById('Password').onkeypress = function (e) {
+        if (!e) e = window.event;
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == '13') {
+            ValidaSesion();
+        }
+    }
+   
+    $("#Ingresar").click(function () {
+        ValidaSesion();
+    });
+});
+// Funcion que valida que los campos sean llenados de manera correcta
+function ValidaSesion() {
+    var user = $("#User").val();
+    var password = $("#Password").val();
+    if (user != "" ) {
+        if( password != ""){
+//           $.ajax({
+//            url: "../Home/ValidaSesion"
+//               , Type: "post"
+//               , dataType: "Json"
+//               , data: { usuario: user, contasena: password }
+//               , success: function (data) {
+//                   var validacion = JSON.parse(data);
+//                   if (validacion == true) {
+//                       window.location.href = "/Principal";
+//                   } else {
+//                       $("#AnswerLogin").html("El usuario o la contaseña son incorrectos.").fadeIn("slow").delay(1500).fadeOut("slow");
+//                   }
+//               }
+//        });  
+        }else{
+             $("#AnswerLogin").html("*Favor de ingresar contaseña.").fadeIn("slow").removeClass("hidden").delay(1500).slideToggle("slow");            
+        } 
+    } else {
+        $("#AnswerLogin").html("*Favor de ingresar usuario.").fadeIn("slow").removeClass("hidden").delay(1500).slideToggle("slow");
+    }
+}
