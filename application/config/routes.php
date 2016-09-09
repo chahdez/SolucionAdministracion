dec88bf9f7d183e037d895f2bc2b4bde9923c2ad
Carlos Herandez <joecarlos2011@gmail.com>
2016-09-08 20:54:02 -0500

    Revert "Carlos - Se añade estructura API"
    This reverts commit d30435552edf991fbca90675e9881b5dfd72f3a3.

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,9 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Login';
-$route['Home'] = 'Home';
-$route['Login'] = 'Login';
-$route['Comparativos'] = 'API/comparativos';
+$route['default_controller'] = 'home';
+$route['Home'] = 'home';
+$route['Login'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
