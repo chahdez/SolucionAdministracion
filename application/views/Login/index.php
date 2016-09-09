51b24645fb77dd9e92318b2612e26b5dd6d3bb33
Ariel Meneses García <Ariel Meneses Garca@192.168.1.74>
2016-09-09 01:38:24 -0500

    Ariel - Se quita una diagonal mal puesta

--- application/views/Login/index.php
+++ application/views/Login/index.php
@@ -7,7 +7,7 @@
  */
 
 ?>
-/*
+
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <script src="libs/js/jquery.js" type="text/javascript"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
