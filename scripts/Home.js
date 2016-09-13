90d76d425a9cfd931ea7b32adbbee39db6155cac
Ariel Meneses García <Ariel Meneses Garca@192.168.1.155>
2016-09-12 22:49:54 -0500

    Ariel - JS terminado con la partend e agregar un neuvo comparaivo

--- scripts/Home.js
+++ scripts/Home.js
@@ -1,13 +1,31 @@
 $(document).ready(function() {
-  $('#example').DataTable( {
-        "ajax": "API/comparativos"
-    } );
+CargaInformacionComparativos(); // Funcion que carga la tabla d elos comparativos
+    $("#AgregarComparativo").click(function (){        
+           $.ajax({
+                url:"../API/NuevoComparativo" ,
+                type: "POST",
+                dataType: "JSON", 
+                data: { Titulo : $("#TituloComparativo").val()}
+              }).done(function( data) {
+                  $("#TituloComparativo").val("");
+                  $("#CancelarTitulo").click();                            
+                 //CargaInformacionComparativos();
+            });
+    });
 } );
 
+//Funcion que carga la tabla de registros de coparativos
+function CargaInformacionComparativos(){
+      $('#example').DataTable( {
+        "ajax": "../API/comparativos"
+    } );
+}
+// Funcion que nos lleva a ver los comparativos
 function VerComparativo(ComparativoID){
     localStorage.setItem("ComparativoID", ComparativoID); // Asignamos en SesionStore el comparativo
     window.location.href = "Comparativo/Ver";
 }
+// Funcion que nos lleva a editar los comparativos
 function EditarComparativo(){
     localStorage.setItem("ComparativoID", ComparativoID); // Asignamos en SesionStore el comparativo
     window.location.href = "Comparativo/Editar";
