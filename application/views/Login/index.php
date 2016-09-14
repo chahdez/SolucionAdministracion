4547925c8d37d2c1938bc8d7ce9b05023ad30010
Ariel Meneses García <Ariel Meneses Garca@192.168.1.155>
2016-09-13 23:29:00 -0500

    Ariel - Se agrega formato de index-login

--- application/views/Login/index.php
+++ application/views/Login/index.php
@@ -2,26 +2,57 @@
 <link href="libs/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 <script src="libs/js/jquery.js" type="text/javascript"></script>
 <script src="scripts/login.js"></script>
-
-<div class="container ">
-    <div class="row ">
-           <div class="col-md-offset-5 col-md-3 well">
-             <form>
-                <div class="form-group">
+<style>
+    body{ 
+        background-color: aliceblue;
+    }
+    .formato{        
+            margin-left: 420px;
+            padding-top: 123px;
+            -moz-box-shadow: 0px 0px 102px -4px rgba(0,0,0,0.46);
+    }
+    .formFormato{
+                height: 350px;
+                width: 550px;
+                background-color: #a9f5c4;
+                color: white;
+                -webkit-box-shadow: 0px 0px 102px -4px rgba(0,0,0,0.46);
+                -moz-box-shadow: 0px 0px 102px -4px rgba(0,0,0,0.46);
+                box-shadow: 0px 0px 102px -4px rgba(0,0,0,0.46);
+                border-radius: 11px;
+                }
+                .formAlineacion{
+                        padding-left: 70px;
+                        padding-right: 70px;
+                        }
+                        .formFormGroup1{
+                                padding-top: 45px;
+                        }
+                        .formButon{
+                                padding-top: 25px;
+                                padding-left: 42px;
+                        }
+                        label{
+                            font-size: x-large;
+                        }
+</style>
+<div class="container formato">
+ 
+           <div class="col-md-3 formFormato">
+               <form class="formAlineacion">
+                <div class="form-group formFormGroup1">
                   <label for="exampleInputEmail1">Usuario:</label>
                   <input type="text" class="form-control" id="User" placeholder="Usuario">
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Contraseña:</label>
-                  <input type="password" class="form-control" id="Password" placeholder="Contarseña">
+                  <input type="password" class="form-control" id="Password" placeholder="Contraseña">
                 </div>
-                <div class="form-group col-lg-offset-7">
+                <div class="form-group col-lg-offset-7 formButon">
                     <button id="Ingresar" type="button" class="btn btn-success">Iniciar Sesion</button>
                 </div>
                  <div id="AnswerLogin" class="alert alert-danger hidden subFormatLogin"></div>
               </form>
         
         </div>
-     
-    </div>
 </div>
